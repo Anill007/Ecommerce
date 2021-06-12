488a5ca6ffcb650d3c7284c6fa4578f92011e408
Anil_Banskota <anil.123aaaaaa@gmail.com>
2021-06-12 14:06:42 +0545

    check errors

--- Buyer/admin.py
+++ Buyer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Buyer, Cart
+from .models import Buyer, Cart, Order
 # Register your models here.
 
-admin.site.register([Buyer, Cart])
+admin.site.register([Buyer, Cart, Order])
