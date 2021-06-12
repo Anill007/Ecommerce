fababcf3fa18d00ff19ceb206946726e56b17aa5
Anil_Banskota <anil.123aaaaaa@gmail.com>
2021-06-12 13:38:49 +0545

    static and media files

--- Ecom/wsgi.py
+++ Ecom/wsgi.py
@@ -15,4 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecom.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root='staticfiles')
-# application.add_files('/path/to/more/static/files', prefix='more-files/')
